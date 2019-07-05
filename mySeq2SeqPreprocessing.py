aee68e9dcbcbd3ed6d726b8fed648ba1c6edbf80
lithium710 <leonard.wt.so@gmail.com>
2019-07-05 11:45:34 +0800

    Updated separated python files.

---
+++ mySeq2SeqPreprocessing.py
@@ -0,0 +1,118 @@
+#!/usr/bin/python
+
+from __future__ import unicode_literals, print_function, division
+import os
+from io import open
+import re
+import torch
+from torch.utils.data import Dataset
+
+MAX_LENGTH = 300
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+SOS_token = 0
+EOS_token = 1
+
+
+class Lang:
+    def __init__(self, name):
+        self.name = name
+        self.word2index = {}
+        self.word2count = {}
+        self.index2word = {0: "SOS", 1: "EOS"}
+        self.n_words = 2  # Count SOS and EOS
+
+    def addSentence(self, sentence):
+        for word in sentence.split(' '):
+            self.addWord(word)
+
+    def addWord(self, word):
+        if word not in self.word2index:
+            self.word2index[word] = self.n_words
+            self.word2count[word] = 1
+            self.index2word[self.n_words] = word
+            self.n_words += 1
+        else:
+            self.word2count[word] += 1
+
+# Lowercase, trim, and remove non-letter characters
+
+
+def normalizeString(s, counter):
+    if counter%2 == 0:
+        s = s.lower().strip()
+        s = re.sub(r"([。,.!?])", r" \1", s)
+        s = re.sub(r"[^a-zA-Z.!?]+", r" ", s)
+    else:
+        s = s.strip()
+        s = " ".join(s)
+    counter += 1
+    return s
+
+
+def readLangs(lang1, lang2, reverse=False):
+    print("Reading lines...")
+    count = 0
+    # Read the file and split into lines
+    lines = open('data/%s-%s.txt' % (lang1, lang2), encoding='utf-8').\
+        read().strip().split('\n')
+
+    # Split every line into pairs and normalize
+    pairs = []
+    for line in lines:
+        new_line = []
+        for s in line.split('\t'):
+            new_line.append(normalizeString(s, count))
+            count += 1
+        pairs.append(new_line)
+    # Reverse pairs, make Lang instances
+    if reverse:
+        pairs = [list(reversed(p)) for p in pairs]
+        input_lang = Lang(lang2)
+        output_lang = Lang(lang1)
+    else:
+        input_lang = Lang(lang1)
+        output_lang = Lang(lang2)
+
+    return input_lang, output_lang, pairs
+
+MAX_LENGTH = 300
+
+
+def filterPair(p):
+    return len(p[0].split(' ')) < MAX_LENGTH and \
+        len(p[1].split(' ')) < MAX_LENGTH
+
+
+def filterPairs(pairs):
+    return [pair for pair in pairs if filterPair(pair)]
+
+def prepareData(lang1, lang2, reverse=False):
+    input_lang, output_lang, pairs = readLangs(lang1, lang2, reverse)
+    print("Read %s sentence pairs" % len(pairs))
+    filterPairs(pairs)
+    for pair in pairs:
+        input_lang.addSentence(pair[0])
+        output_lang.addSentence(pair[1])
+    print("Counted words:")
+    print(input_lang.name, input_lang.n_words)
+    print(output_lang.name, output_lang.n_words)
+    return input_lang, output_lang, pairs
+
+def indexesFromSentence(lang, sentence):
+    return [lang.word2index[word] for word in sentence.split(' ')]
+
+
+def tensorFromSentence(lang, sentence):
+    indexes = indexesFromSentence(lang, sentence)
+    indexes.append(EOS_token)
+    return torch.tensor(indexes, dtype=torch.long, device=device).view(-1, 1)
+
+class SentenceDataset(Dataset):
+    def __init__(self, pairs):
+        self.mypairs = pairs
+
+    def __len__(self):
+        return len(self.mypairs)
+
+    def __getitem__(self, idx):
+        return self.mypairs[idx]
